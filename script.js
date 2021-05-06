c908a964c4a668971e3d520e3ee580882dda035b
Rewa Patil <>
2021-05-06 19:53:33 +0000

    js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ function myFunction () {
   const num1 = parseInt(document.getElementById('base1').value)
   const num2 = parseInt(document.getElementById('base2').value)
   const num3 = parseInt(document.getElementById('height').value)
-  <!--formula for calculation-->
+
   const ans = (num1 + num2)/2 * num3
     alert(ans)
+
     document.getElementById('displayans').innerText = 'Answer=' + ans
 }
